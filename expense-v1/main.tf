724f27d7ef5501f1f59f8fcea8a742784c403320
aamani <aamani.garipelly@gmail.com>
2024-05-31 20:48:02 +0530

    test

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -6,6 +6,15 @@ resource "aws_instance" "frontend" {
     name ="frontend"
   }
 }
+
+resource "aws_route53_record" "frontend" {
+  zone_id = Z0529499O193BZWWIZRO
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "backend" {
   ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
@@ -14,6 +23,15 @@ resource "aws_instance" "backend" {
     name ="backend"
   }
 }
+
+resource "aws_route53_record" "backend" {
+  zone_id = Z0529499O193BZWWIZRO
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
@@ -21,4 +39,12 @@ resource "aws_instance" "mysql" {
   tags = {
     name ="mysql"
   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = Z0529499O193BZWWIZRO
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
